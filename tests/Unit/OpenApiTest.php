4cb5b26984b05246466b1e6772c03a011b9aab82
PokemonGo Lingen <75855418+mario6700-pogo@users.noreply.github.com>
2021-04-05 16:41:53 +0200

    Validate openAPI Spec

---
+++ tests/Unit/OpenApiTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PokemonGoLingen\PogoAPI;
+
+use GuzzleHttp\Psr7\Response;
+use League\OpenAPIValidation\PSR7\Exception\ValidationFailed;
+use League\OpenAPIValidation\PSR7\OperationAddress;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
+use League\OpenAPIValidation\Schema\Exception\KeywordMismatch;
+use PHPUnit\Framework\TestCase;
+
+use function basename;
+use function file_get_contents;
+use function glob;
+use function implode;
+use function realpath;
+use function sprintf;
+
+/**
+ * @coversNothing
+ */
+class OpenApiTest extends TestCase
+{
+    /**
+     * @dataProvider routesDataProvider
+     */
+    public function testOpenAPISpecification(string $filePath, string $apiRoute): void
+    {
+        $this->expectNotToPerformAssertions();
+        $openApiFile = __DIR__ . '/../../public/Pokedex.yaml';
+        $validator   = (new ValidatorBuilder())->fromYamlFile($openApiFile)->getResponseValidator();
+
+        $responseContent = file_get_contents($filePath);
+        if ($responseContent === false) {
+            self::fail(sprintf('Cant read file "%s"', $filePath));
+        }
+
+        $response = new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $responseContent
+        );
+        try {
+            $validator->validate(
+                new OperationAddress(
+                    $apiRoute,
+                    'get'
+                ),
+                $response
+            );
+        } catch (ValidationFailed $e) {
+            $previous = $e->getPrevious();
+
+            if ($previous instanceof KeywordMismatch) {
+                $failMessage = $previous->getMessage();
+
+                if ($previous->dataBreadCrumb() !== null) {
+                    $failMessage .= '[' . implode('.', $previous->dataBreadCrumb()->buildChain()) . ']';
+                }
+
+                self::fail($failMessage);
+            }
+
+            throw $e;
+        }
+    }
+
+    /**
+     * @return array<string, string[]>
+     */
+    public function routesDataProvider(): iterable
+    {
+        $files = glob(__DIR__ . '/../../data/tmp/api/*.json') ?: [];
+        foreach ($files as $file) {
+            $filePath = realpath($file);
+            if ($filePath === false) {
+                continue;
+            }
+
+            $fileName = basename($filePath) ?: $filePath;
+
+            yield $fileName => [$filePath, '/api/' . $fileName];
+        }
+    }
+}
