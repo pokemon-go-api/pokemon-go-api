079a7c913e5cb2d860ee413e43846c8934cf065c
mario6700-pogo <75855418+mario6700-pogo@users.noreply.github.com>
2023-06-19 22:52:21 +0200

    Fix failing build

--- src/Types/PokemonFormCollection.php
+++ src/Types/PokemonFormCollection.php
@@ -42,7 +42,7 @@ final class PokemonFormCollection
                 continue;
             }
 
-            $formOnlyId = str_replace($templateIdParts['name'] . '_', '', (string)$formData->form);
+            $formOnlyId = (string)str_replace($templateIdParts['name'] . '_', '', (string)$formData->form);
 
             $forms[] = new PokemonForm(
                 $formData->form,
