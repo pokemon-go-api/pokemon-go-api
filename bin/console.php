66b7f3736f2952cc4d90730488632e21f96bbe85
mario6700-pogo <75855418+mario6700-pogo@users.noreply.github.com>
2025-01-02 17:39:47 +0100

    Skip old usage of set-output in github

--- bin/console.php
+++ bin/console.php
@@ -281,5 +281,6 @@ file_put_contents(
     CSS,
 );
 $hasChanges = $cacheLoader->hasChanges();
-$logger->debug(sprintf('CACHE_STATUS=%s', $hasChanges ? 'HAS_CHANGES' : 'NO_CHANGES'));
-echo sprintf('::set-output name=CACHE_STATUS::%s' . PHP_EOL, $hasChanges ? 'HAS_CHANGES' : 'NO_CHANGES');
+$cacheStatus = $hasChanges ? 'HAS_CHANGES' : 'NO_CHANGES';
+$logger->debug(sprintf('CACHE_STATUS=%s', $cacheStatus));
+exec('echo "CACHE_STATUS='.$cacheStatus.'" >> $GITHUB_OUTPUT');
